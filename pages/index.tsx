c0d48cb6fce1a29bcffe8fd278fd1e21549a83bc
Thales Nunes <thalesgog@hotmail.com>
2021-02-13 18:49:22 -0300

    Include nextauth for authentication solution

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,37 @@
-import {NextPage} from 'next'
+import {GetServerSideProps, NextPage} from 'next'
+import {getSession, signOut, useSession} from 'next-auth/client'
 
-const IndexPage: NextPage = () => <h1>Dragons App</h1>
+const IndexPage: NextPage = () => {
+  const [session] = useSession()
+
+  return (
+    <>
+      <h1>Dragons App</h1>
+      {session && (
+        <>
+          Signed in as {session.user.email} <br />
+          <button onClick={() => signOut()}>Sign out</button>
+        </>
+      )}
+    </>
+  )
+}
 
 export default IndexPage
+
+export const getServerSideProps: GetServerSideProps = async ctx => {
+  const session = await getSession(ctx)
+
+  if (!session) {
+    return {
+      redirect: {
+        destination: '/api/auth/signin',
+        permanent: false,
+      },
+    }
+  }
+
+  return {
+    props: {},
+  }
+}
